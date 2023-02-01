48640e1c0d80a2bc929f54c7f5171d22aa8e87f5
Atakan Akkan <85180975+AtakanAKKAN@users.noreply.github.com>
2023-02-01 16:48:18 +0300

    -

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 $(document).click(function(e) {
    e.preventDefault();
  });
+
+ window.sr = ScrollReveal();
+
+ sr.reveal(".ani-left",{
+      origin:'left',
+      duration:1750,
+      distance: '8rem',
+      delay:600,
+      
+
+ });
+
+ sr.reveal(".ani-right",{
+  origin:'right',
+  duration:1750,
+  distance: '2%',
+  delay:600,
+ 
+});
+
+sr.reveal(".ani-top",{
+  origin:'top',
+  duration:1500,
+  distance: '8rem',
+  delay:500,
+
+});
+
+sr.reveal(".ani-bottom",{
+  origin:'bottom',
+  duration:1500,
+  distance: '8rem',
+  delay:500,
+
+});
